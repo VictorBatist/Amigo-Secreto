af6d2479659f1bf64c506da80a070799c80786d2
Victor Batista <vbatista.dev@gmail.com>
2025-02-14 14:46:52 -0300

    Adicionando a função de sortear os numeros

--- app.js
+++ app.js
@@ -11,12 +11,43 @@ function adicionarAmigo(){
     }else{
         listaDeAmigos.push(nomeDoAmigo);
         console.log(listaDeAmigos);
+        exibirAmigosNaTela();
     }
     
-    limparCampo();
-   
+    limparCampo();   
 }
+//função de exibir o nome dos amigos na tela
+function exibirAmigosNaTela(){ 
+    let lista = document.getElementById('listaAmigos'); 
+    lista.innerHTML = " ";
+        
+    for(let i = 0; i <= listaDeAmigos.length; i++){
+        let nome = document.createElement('li');
+        nome.textContent = listaDeAmigos[i];
+        lista.appendChild(nome);
+    }
+}
+
+function sortearAmigo(){
+    let resultadoDoSorteio = document.getElementById('resultado');
+    if(listaDeAmigos == []){
+        alert('Por favor, insira um nome');
+    }else{
+        // escolhe um indice Aleatorio e consecutivamente um nome aleatorio no Array 
+        let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+        let nomeSorteado = listaDeAmigos[indiceAleatorio];
+        console.log(nomeSorteado);
+
+        // imprimi o resultado do nome sorteado na tela
+        let resultado = document.createElement('li');
+        resultado.textContent = `Seu Amigo Secreto é: ${nomeSorteado}`;
+        resultadoDoSorteio.appendChild(resultado);
+    }
+}
+
 // função limpar campo do input 
 function limparCampo(){
     document.querySelector('input').value = '';
 }
+
+
